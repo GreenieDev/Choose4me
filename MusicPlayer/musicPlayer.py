4ec3d57f2cc434cc032558640f39a7a09af92f85
GreenieDev <f.drive.12.db@gmail.com>
2021-07-07 18:43:57 +0100

    Project 5 (Music Player - NoGUI)

--- MusicPlayer/musicPlayer.py
+++ MusicPlayer/musicPlayer.py
@@ -17,31 +17,42 @@ class MusicPlayer:
         while self.stop == False:
             self.playing = 1
             aP.Apresentar("Music Player", self.texto1)
+
             self.pedirMusica()
-            if self.stop == False:
-                if self.playing == 1:
-                    winsound.PlaySound(f"musics\\{self.music}", winsound.SND_ALIAS)
-                    #print("Acabou!")
-                os.system("cls")
-                    
 
+            os.system("cls")
 
+                            
     def pedirMusica(self):
         
         files, options = opt.Options()
+        self.playlist = False
 
         print(options)
-        try:
-            musicIndex = (input("Select yout option:   "))
-            if musicIndex.upper() == "X":
-                os.system("cls")
-                self.stop = True
-            else:
+        PlaylistIndexes = (input("Select yout option:   "))
+        if PlaylistIndexes.lower() != "x":
+            loop = input("Loop? (Y/n)    ")
+            while True:
                 
-                self.music = files[int(musicIndex)]
-        except:
-            print("Something went wrong, make sure you selected a valid option!")
-            self.playing = 0
-            self.options = ""
+                    for musicI in PlaylistIndexes:
+                        try:
+                            self.music = files[int(musicI)]
+                            self.Tocar()
+                        except:
+                            print("Something went wrong, make sure you selected a valid option!")
+                            self.playing = 0
+                            self.options = ""
+                    if loop.lower() != "y":
+                        break
+        else:
+            self.stop = True
+            os.system("cls")
+             
+        
+                
+                
+    def Tocar(self):
+        winsound.PlaySound(f"musics\\{self.music}", winsound.SND_ALIAS)
+
 player = MusicPlayer()
 player.iniciar()
